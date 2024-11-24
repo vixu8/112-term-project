9b35e71eaa8d9032d9d06b95b7fa5032438d57a4
vixu8 <viola.r.xu@gmail.com>
2024-11-24 18:38:04 -0500

    simple game working
    switched to simpleaudio
    added 8 cols instead
    workin gnote/tap sensing

--- note.py
+++ note.py
@@ -1,14 +1,22 @@
 class Note:
-    width = 50
-    height = 20
+    screenHeight = 600
+    width = 1400/8
+    height = 700/13
 
-    def __init__(self, col, type, timestamp):
+    def __init__(self, col, type, percent):
         self.col = col
         self.type = type
-        self.timeUntil = timestamp
-        self.y = 50
+        self.percent = percent
+        self.y = Note.screenHeight-(Note.screenHeight*(self.percent/100))
         #self.y = something  based on time until
         self.x = 0
+
+        if self.percent < 100:
+            self.drawn = True
+        else: self.drawn = False
+
+    def __repr__(self):
+        return f"Note, col {self.col}, at {self.percent}%"
         
     def giveCoordByPercent(startX, startY, endX, endY, percent):
         lengthX = (endX-startX)
@@ -18,4 +26,8 @@ class Note:
 
     def move(self, px):
         self.y += px
+        self.percent = (-1*(self.y -Note.screenHeight)/Note.screenHeight)*100
+        if self.percent < 100 and self.percent > -20:
+            self.drawn = True
+        else: self.drawn = False
     
